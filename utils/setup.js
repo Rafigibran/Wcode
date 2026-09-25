ac463c503d93027e7de60f8de55254557389920b
SpaceVise <57510700+Rafigibran@users.noreply.github.com>
2026-09-25 16:48:00 +0800

    Always install AdMob plugin

--- utils/setup.js
+++ utils/setup.js
@@ -13,26 +13,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const PLATFORM_FILES = [".DS_Store"];
 const PACKAGE_MANAGERS = new Set(["bun", "npm", "pnpm", "yarn"]);
-const ID_PAID = "com.wizardnative.wcode";
-const ADMOB_PLUGIN_DIR = "admob";
 const REPO_ROOT = path.join(__dirname, "..");
-
-function isPaidVersion() {
-	const configPath = path.join(__dirname, "../config.xml");
-	let config;
-
-	try {
-		config = fs.readFileSync(configPath, "utf8");
-	} catch (error) {
-		throw new Error(`Unable to read config.xml at ${configPath}.`, {
-			cause: error,
-		});
-	}
-
-	const widgetId = /<widget[^>]*?\sid=["']([^"']+)["']/.exec(config)?.[1];
-
-	return widgetId === ID_PAID;
-}
 
 function getPackageManager() {
 	const userAgent = process.env.npm_config_user_agent;
@@ -179,13 +160,11 @@ function main() {
 	execSync("cordova plugin add cordova-plugin-device", { stdio: "inherit" });
 	execSync("cordova plugin add cordova-plugin-file", { stdio: "inherit" });
 
-	const shouldSkipAdmob = isPaidVersion();
 	const plugins = fs.readdirSync(path.join(__dirname, "../src/plugins"));
 	plugins.forEach((plugin) => {
 		if (PLATFORM_FILES.includes(plugin) || plugin.startsWith(".")) return;
 		const pluginPath = path.join(__dirname, "../src/plugins", plugin);
 		if (!fs.lstatSync(pluginPath).isDirectory()) return;
-		if (shouldSkipAdmob && plugin === ADMOB_PLUGIN_DIR) return;
 		execSync(`cordova plugin add ./src/plugins/${plugin}`, {
 			stdio: "inherit",
 		});
